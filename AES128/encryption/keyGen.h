02d325c5664678cb5698000e3e630d3345607476
Mark Magdy <markgeforce4080@gmail.com>
2025-02-02 19:05:01 +0200

    updateDirectories

--- AES128/encryption/keyGen.h
+++
@@ -1,9 +0,0 @@
-#ifndef AES_KEYGEN_H
-#define AES_KEYGEN_H
-#include "../aes128_helper/aes128_helper.h"
-using namespace AES128;
-class KeyGen {
-public:
-    static matrix gen_key(const matrix& keyMat, const int round);
-};
-#endif
